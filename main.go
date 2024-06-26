36d545fba56cf75939a570a0d2431860771751e7
junta <juntakoman123@gmail.com>
2024-06-26 12:20:12 +0900

    変数名修正

--- main.go
+++ main.go
@@ -16,30 +16,32 @@ func main() {
 		log.Printf("need port number\n")
 		os.Exit(1)
 	}
-	p := os.Args[1]
-	l, err := net.Listen("tcp", ":"+p)
+
+	port := os.Args[1]
+	listener, err := net.Listen("tcp", ":"+port)
 
 	if err != nil {
-		log.Fatalf("failed to listen port %s: %v", p, err)
+		log.Fatalf("failed to listen port %s: %v", port, err)
 	}
 
-	if err := run(context.Background(), l); err != nil {
+	err = run(context.Background(), listener)
+
+	if err != nil {
 		log.Printf("Failed to terminate server: %v", err)
 		os.Exit(1)
 	}
 }
 
-func run(ctx context.Context, l net.Listener) error {
-	s := &http.Server{
+func run(ctx context.Context, listener net.Listener) error {
+	server := &http.Server{
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
 	}
 	eg, ctx := errgroup.WithContext(ctx)
 
-	// 別ゴルーチンでhttpサーバーを起動する
 	eg.Go(func() error {
-		if err := s.Serve(l); err != nil &&
+		if err := server.Serve(listener); err != nil &&
 			err != http.ErrServerClosed {
 			log.Printf("failed to close: %+v", err)
 			return err
@@ -47,9 +49,8 @@ func run(ctx context.Context, l net.Listener) error {
 		return nil
 	})
 
-	// チャネルからの通知を待機する
 	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
+	if err := server.Shutdown(context.Background()); err != nil {
 		log.Printf("failed to shutdown: %+v", err)
 	}
 
